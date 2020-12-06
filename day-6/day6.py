7acb6e63cf3251959144a34c1ea3211065c782a0
Marie Crappe <mcrappe@veepee.com>
2020-12-06 23:46:41 +0100

    Improvement intersection - solution day 6

--- day-6/day6.py
+++ day-6/day6.py
@@ -1,13 +1,11 @@
 if __name__ == '__main__':
     with open("input.txt") as answers_file:
         groups = answers_file.read().split('\n\n')
-        first_answer = 0
-        second_answer = 0
+        first_solution = 0
+        second_solution = 0
         for group in groups:
-            group_answers = ''.join(group.split('\n'))
-            first_answer += len("".join(set(group_answers)))
-            common_answers = set(group.split('\n')[0])
-            for answer in group.split('\n'):
-                common_answers = common_answers.intersection(set(answer))
-            second_answer += len(common_answers)
-        print(first_answer, second_answer)
+            group_answers = group.split('\n')
+            first_solution += len("".join(set(''.join(group_answers))))
+            common_answers = set.intersection(*(set(line) for line in group_answers))
+            second_solution += len(common_answers)
+        print(first_solution, second_solution)
